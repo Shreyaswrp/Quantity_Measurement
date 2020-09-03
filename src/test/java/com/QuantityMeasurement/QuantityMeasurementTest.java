febfbf7ec34f926db301032567c0ec3ad034f028
Shreya Swaroop <shreyaswrp@gmail.com>
2020-09-03 17:06:54 +0530

    Refactored to check if the values of 2 given lengths are same or not

--- src/test/java/com/QuantityMeasurement/QuantityMeasurementTest.java
+++ src/test/java/com/QuantityMeasurement/QuantityMeasurementTest.java
@@ -17,15 +17,32 @@ public class QuantityMeasurementTest {
     public void given0feetand0feet_ShouldReturnEqual(){
         Length feet1 = new Length(Unit.FEET,0);
         Length feet2 = new Length(Unit.FEET,0);
-        Assert.assertEquals(feet1,feet2);
+        boolean equals = feet1.equals(feet2);
+        Assert.assertTrue(equals);
+    }
+
+    @Test
+    public void given0feetand1feet_ShouldReturnNotEqual(){
+        Length feet1 = new Length(Unit.FEET,0);
+        Length feet2 = new Length(Unit.FEET,1);
+        boolean equals = feet1.equals(feet2);
+        Assert.assertFalse(equals);
     }
 
     @Test
     public void given0Inchand0Inch_ShouldReturnEqual(){
         Length inch1 = new Length(Unit.INCH,0);
         Length inch2 = new Length(Unit.INCH,0);
-        Assert.assertEquals(inch1,inch2);
+        boolean equals = inch1.equals(inch2);
+        Assert.assertTrue(equals);
     }
 
+    @Test
+    public void given0Inchand1Inch_ShouldReturnNotEqual(){
+        Length inch1 = new Length(Unit.INCH,0);
+        Length inch2 = new Length(Unit.INCH,1);
+        boolean equals = inch1.equals(inch2);
+        Assert.assertFalse(equals);
+    }
 
 }
